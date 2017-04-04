3cea26663ee8c2619716a62b2422f1cb2cb94862
pbpoon <pbpoon@live.com>
2017-04-05 00:49:42 +0800

    开始做asset的数据导入,差不多完成

--- apps/asset/admin.py
+++ apps/asset/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django import forms
+from .models import Area, Category, LandNum, Owner
 
-# Register your models here.
+
+class InlineAreaLandNum(admin.TabularInline):
+    model = LandNum
+    # raw_id_fields = ('area',)
+
+
+class InlineOwnerLandNum(admin.StackedInline):
+    model = LandNum.owner.through
+    raw_id_fields = ('owner',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'desc']
+    search_fields = ['name', 'desc']
+    inlines = [InlineAreaLandNum]
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    inlines = [InlineOwnerLandNum]
