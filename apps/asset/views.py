233f666213278f4d217e530e46901c22aaf7be08
pbpoon <pbpoon@live.com>
2017-04-05 18:07:05 +0800

    重新调试田地数据录入

--- apps/asset/views.py
+++ apps/asset/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from account.forms import FileForm
-from .models import Area, Category, LandNum
-from  account.models import People
+from .models import Area, Category, LandNum, Owner
+from account.models import People
 import xlrd
 import decimal
 
@@ -21,23 +21,25 @@ class FileUploadView(FormView):
             colnames = table.row_values(0)  # 表头列名称数据
             print(colnames)
             list = []
+            list2 = []
             area = [Area(name=str(x)) for x in set(table.col_values(2, 1))]
             categroy = [Category(name=str(x)) for x in set(table.col_values(1, 1))]
-            print(area)
-            Area.objects.bulk_create(area)
-            Category.objects.bulk_create(categroy)
+            # print(area)
+            # Area.objects.bulk_create(area)
+            # Category.objects.bulk_create(categroy)
             for rownum in range(1, nrows):
                 row = table.row_values(rownum)
 
                 for index, i in enumerate(range(len(colnames))):
                     if row:
                         if index == 0:
-                            people =People.objects.all()
+                            people = People.objects.all()
                             sname = str(row[i])
-                            sname = sname[:1] + ' ' + sname[1:6]
-                            if sname in people:
+                            if people.get(first_name=sname[:1], last_name=sname[1:6]):
                                 row[i] = people.get(first_name=sname[:1], last_name=sname[2:6])
-                                row[99] = str(row[i])
+                            else:
+                                row[i] = None
+                            row[7] = row[i]
                         elif index == 1:
                             row[i] = Category.objects.get(name=str(row[i]))
                         elif index == 2:
@@ -45,17 +47,22 @@ class FileUploadView(FormView):
                         elif index == 4:
                             row[i] = decimal.Decimal(row[i])
                         elif index == 6:
-                            row[i] = row[5] + '--编辑备注:' + str(row[i])
+                            if row[i]:
+                                row[i] = row[5] + '--编辑备注:' + str(row[i])
+                            else:
+                                row[i] = row[5]
                         else:
                             row[i] = str(row[i])
 
-                if not LandNum.objects.filter(area=row[2], num=row[3]):
-                    list.append(LandNum(area=row[2], num=row[3], fm=row[4], owner=row[0],
-                                        old_owner=row[99], ps=row[6]))
+
+                # if not LandNum.objects.filter(area=row[2], num=row[3]):
+                land = LandNum(area=row[2], num=row[3], category=row[1], fm=row[4], owner=row[0], ps=row[6])
+                land.save()
+                owner = Owner(owner=row[0], old_owner=row[7], num=land, ps=row[6])
+                owner.save()
 
 
 
-
-                print(list)
+            print('OK')
                 # account = row[10],
         # People.objects.bulk_create(list)
